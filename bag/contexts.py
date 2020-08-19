0518f8cce0ab7c5ed8c38735cbc6d3c13f9562e7
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-19 15:23:29 +0200

    initial shopping bag functionality. - context: docstring + calculator for totals etc - bag page: basic layout to display items the customer has selected

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,32 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
-    """ Context for shopping bag for use cross-site """
+    """
+        Context for shopping bag for use cross-site
+        - Access anywhere
+        - Calculates shipping & free shipping threashold
+        - Sses session to store and calculate users purchases
+        - Returns data + product objects
+    """
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product
+        })
 
     if total < settings.FREE_DELIVERY_THREASHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
