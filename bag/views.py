a59496555a897c54f40d06a3ddbbe97d64bed681
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-20 11:23:58 +0200

    adjusting view & context to handle item sizes - Storing size data as a dictionary - Re-name quantity to item_data for clarity

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,30 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+
+    # Capturing size
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # Structure the bad depending on whether the product has sizes
+    # If sizes, store as a dictionary where key is size and value is quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+
+    # If the item doesn't have size
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
 
