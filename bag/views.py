8c48ba09375dd9251396d9db49f15381572e5001
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-20 20:03:08 +0200

    set up & test for toasts - HTML written and tested with single working sucess toast

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect, reverse, HttpResponse
+
+from products.models import Product
 
 
 def view_bag(request):
@@ -9,6 +12,8 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ A view to add items to the user's shoping bag"""
+
+    product = Product.objects.get(pk=item_id)
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -36,6 +41,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added { product.name } to your bag!')
 
     request.session['bag'] = bag
 
