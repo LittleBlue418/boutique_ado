6073e5559360dc0b4c0c5a098cc33ff832163c98
LittleBlue418 <hollyathomas88@gmail.com>
2020-08-26 14:50:26 +0200

    Added checkout form data caching in payment intent

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), '127.0.0.1']
+# ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS"), '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
